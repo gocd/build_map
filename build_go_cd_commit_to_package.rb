396b16ae27986526db087acc5217196348715916
Chad Wilson <29788154+chadlwilson@users.noreply.github.com>
2026-01-29 11:40:32 +0800

    Simplify script

--- build_go_cd_commit_to_package.rb
+++ build_go_cd_commit_to_package.rb
@@ -1,33 +1,16 @@
 #!/usr/bin/env ruby
-
-require 'rubygems'
-require 'rake'
-%w(rake rake/file_utils).each do |f|
-  begin
-    require f
-  rescue LoadError => e
-    puts "Unable to require #{f}, continuing"
-  end
-end
+require 'fileutils'
+include FileUtils
 
 $stdout.sync = true
 $stderr.sync = true
-
-include FileUtils
 
 unless(ENV['UPDATE_GOCD_BUILD_MAP'])
   puts "Not updating build_map repo as Environment variable UPDATE_GOCD_BUILD_MAP is not set, exiting.."
   exit(true)
 end
-rm_rf 'build_map'
 
-sh("git clone https://github.com/gocd/build_map")
-
-at_exit do
-  rm_rf 'build_map'
-end
-
-cd 'build_map' do
+cd __dir__ do
   entry = "#{ENV['GO_TO_REVISION_GOCD']}:#{ENV['GO_PIPELINE_COUNTER']}/#{ENV['GO_STAGE_COUNTER']}"
   File.open('commit_build_map', 'a') do |f|
     f.puts entry
